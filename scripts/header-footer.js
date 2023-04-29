4e65da92ffccc94abcce0a6b496d5dc12eada5e6
Alex <alexrw008@gmail.com>
2023-04-29 01:42:49 -0500

    cleanup some stuff

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -3,7 +3,7 @@ let header = `
         <ul>
             <li><a href="#">alexander wang</a></li>
             <span style="float:right;">
-                <li><a href="https://www.instagram.com/al_xrw/" target="_blank"><img src="svgs/instagram.svg" class="imglink"></a></li>
+                <li><a href="https://www.instagram.com/al_xrw/" target="_blank">as<img src="svgs/instagram.svg" class="imglink"></a></li>
                 <li><a href="mailto:alexrw008@gmail.com" target="_blank"><img src="svgs/mail.svg" class="imglink"></a></li>
             </span>
         </ul>
@@ -11,7 +11,7 @@ let header = `
 `;
 
 let footer = `
-    <p>©mmxxiii</p>
+    <p>©mmxxiii alexander wang</p>
     <a href="https://github.com/yhoundz/yhoundz.github.io" target="_blank">source code</a>
 `;
 
